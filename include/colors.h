2a0f947a67ff9ff1b89635aa237d58a7d77caad2
Esterhasz <smippy098@gmail.com>
2026-08-17 12:12:36 -0600

    add files

---
+++ include/colors.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace color {
+	constexpr char* RESET	= "\033[0m";
+	constexpr char* RED		= "\033[31m";
+	constexpr char* GREEN	= "\033[32m";
+	constexpr char* YELLOW	= "\033[33m";
+	constexpr char* BLUE	= "\033[34m";
+	constexpr char* MAGENTA	= "\033[35m";
+	constexpr char* CYAN	= "\033[36m";
+}
---
+++ src/bitpal.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "colors.h"
+
+using namespace std;
+
+int main()
+{
+	std::cout << "\033[2;31m" << "Red" << "\033[0m" << std::endl;
+	std::cout << "\033[31m" << "Red" << "\033[0m" << std::endl;
+	std::cout << "\033[1;31m" << "Red" << "\033[0m" << std::endl;
+	std::cout << "\033[91m" << "Red" << "\033[0m" << std::endl;
+
+	return 0;
+}
