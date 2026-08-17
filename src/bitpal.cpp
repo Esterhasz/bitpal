d62a00b27fbb7424b11da90e674c2055ac360ebb
Esterhasz <smippy098@gmail.com>
2026-08-17 16:15:02 -0600

    add background colors

--- src/bitpal.cpp
+++ src/bitpal.cpp
@@ -5,10 +5,5 @@ using namespace std;
 
 int main()
 {
-	std::cout << "\033[2;31m" << "Red" << "\033[0m" << std::endl;
-	std::cout << "\033[31m" << "Red" << "\033[0m" << std::endl;
-	std::cout << "\033[1;31m" << "Red" << "\033[0m" << std::endl;
-	std::cout << "\033[91m" << "Red" << "\033[0m" << std::endl;
-
 	return 0;
 }
